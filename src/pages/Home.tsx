d914404f491b283f701f9505198388699f84e38c
nlom0218 <nlom0218@naver.com>
2022-07-26 13:16:32 +0900

    Home Part3 first draft

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,62 @@ const Container = styled.div``;
 const Layout = styled.div`
   display: grid;
   grid-template-columns: 1fr auto;
-  padding: 120px 60px;
+  gap: 40px;
+  gap: 2.5rem;
+  margin: 60px;
+  margin: 3.75rem;
+  align-items: stretch;
 `;
 
-const Contents = styled.div``;
+const LayoutTwo = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 20px;
+`;
 
-const GameImage = styled.img``;
+const Image = styled.div`
+  background: url("https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80");
+  background-size: cover;
+  background-position: center;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  line-height: 160%;
+  div:first-child {
+    margin-bottom: 20px;
+    font-size: 2em;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+  div:last-child {
+    color: ${colors.lightTextColor};
+  }
+`;
+
+const GameImage = styled.img`
+  padding: 0px 20px;
+  padding: 0rem 1.25rem;
+  position: relative;
+  top: -180px;
+`;
+
+const LayoutThree = styled.div`
+  grid-column: 1 / -1;
+  display: grid;
+  grid-template-columns: 2fr 3fr;
+  column-gap: 40px;
+`;
+
+const Image2 = styled.div`
+  position: relative;
+  height: 240%;
+  top: -160px;
+  background-image: url("https://images.unsplash.com/photo-1499540633125-484965b60031?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80");
+  background-size: cover;
+  background-position: center;
+`;
 
 const Home = () => {
   return (
@@ -21,8 +71,31 @@ const Home = () => {
       <Intro />
       <AboutGroup />
       <Layout>
-        <Contents></Contents>
+        <LayoutTwo>
+          <Image></Image>
+          <Contents>
+            <div>CREATIVE</div>
+            <div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloremque aliquam soluta saepe deserunt mollitia natus blanditiis
+              unde explicabo, porro culpa numquam! Quia itaque porro quisquam
+              culpa non tenetur numquam accusantium.
+            </div>
+          </Contents>
+        </LayoutTwo>
         <GameImage src="/img/chess.png" />
+        <LayoutThree>
+          <Contents>
+            <div>ACHIEVEMENT</div>
+            <div>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloremque aliquam soluta saepe deserunt mollitia natus blanditiis
+              unde explicabo, porro culpa numquam! Quia itaque porro quisquam
+              culpa non tenetur numquam accusantium.
+            </div>
+          </Contents>
+          <Image2></Image2>
+        </LayoutThree>
       </Layout>
     </Container>
   );
