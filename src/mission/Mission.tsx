5fba31de1133a5e723ffdbec578a6869a8bc5164
nlom0218 <nlom0218@naver.com>
2022-07-27 16:38:06 +0900

    Mission styled

--- src/mission/Mission.tsx
+++ src/mission/Mission.tsx
@@ -1,13 +1,30 @@
+import styled from "styled-components";
 import MemberCard from "./components/MemberCard";
 import member from "./member";
 
+const Container = styled.div`
+  background: linear-gradient(#8000ff6d, #e100ff6d);
+  min-height: 100vh;
+`;
+
+const MemberList = styled.div`
+  padding: 40px;
+  max-width: 320px;
+  margin: 0 auto;
+  div:not(:last-child) {
+    margin-bottom: 40px;
+  }
+`;
+
 const Mission = () => {
   return (
-    <div>
-      {member.map((item, index) => {
-        return <MemberCard key={index} {...item} />;
-      })}
-    </div>
+    <Container>
+      <MemberList>
+        {member.map((item, index) => {
+          return <MemberCard key={index} {...item} />;
+        })}
+      </MemberList>
+    </Container>
   );
 };
 
