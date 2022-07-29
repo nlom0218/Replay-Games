f985a4f1b39a17d3becf903759a4c3d06b83cc91
nlom0218 <nlom0218@naver.com>
2022-07-29 23:25:01 +0900

    add Flower

--- src/mission/Mission.tsx
+++ src/mission/Mission.tsx
@@ -25,15 +25,32 @@ const Mission = () => {
           return <MemberCard key={index} {...item} />;
         })}
       </MemberList>
-      <Flower color="#ff5500" duration={40} delay={1} left={30} />
-      <Flower color="#ff5500" duration={30} delay={2} left={60} />
+      <Flower color="#ff7c3b" duration={40} delay={1} left={30} />
+      <Flower color="#ff2424" duration={30} delay={2} left={60} />
       <Flower color="#ff5500" duration={60} delay={3} left={90} />
       <Flower color="#f9fd39" duration={60} delay={1} left={10} />
-      <Flower color="#f9fd39" duration={40} delay={2} left={40} />
+      <Flower color="#84fd39" duration={40} delay={2} left={40} />
       <Flower color="#f9fd39" duration={30} delay={3} left={70} />
-      <Flower color="#1b41ff" duration={30} delay={1} left={20} />
+      <Flower color="#1ba0ff" duration={30} delay={1} left={20} />
       <Flower color="#1b41ff" duration={40} delay={2} left={40} />
-      <Flower color="#1b41ff" duration={60} delay={3} left={80} />
+      <Flower color="#ff5500" duration={40} delay={11} left={30} />
+      <Flower color="#ff874b" duration={30} delay={12} left={60} />
+      <Flower color="#ff5500" duration={60} delay={13} left={90} />
+      <Flower color="#f9fd39" duration={60} delay={11} left={10} />
+      <Flower color="#53ff38" duration={40} delay={12} left={40} />
+      <Flower color="#f9fd39" duration={30} delay={13} left={70} />
+      <Flower color="#1b41ff" duration={30} delay={11} left={20} />
+      <Flower color="#1b41ff" duration={40} delay={12} left={40} />
+      <Flower color="#1b41ff" duration={60} delay={20} left={80} />
+      <Flower color="#ff5500" duration={40} delay={21} left={30} />
+      <Flower color="#ff5500" duration={30} delay={22} left={60} />
+      <Flower color="#ff5500" duration={60} delay={23} left={90} />
+      <Flower color="#f9fd39" duration={60} delay={21} left={10} />
+      <Flower color="#f9fd39" duration={40} delay={22} left={40} />
+      <Flower color="#f9fd39" duration={30} delay={23} left={70} />
+      <Flower color="#1b41ff" duration={30} delay={21} left={20} />
+      <Flower color="#1b41ff" duration={40} delay={22} left={40} />
+      <Flower color="#1b41ff" duration={60} delay={25} left={80} />
     </Container>
   );
 };
